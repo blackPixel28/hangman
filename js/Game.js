a7f61538b8f31f808150836658b71bf374542666
Michal Snuszka <misiek28@gmail.com>
2021-03-25 12:46:50 +0100

    add getLetter

--- js/Game.js
+++ js/Game.js
@@ -1,9 +1,22 @@
 class Game {
-  constructor() {}
+  constructor({ word, letter }) {
+    this.word = word;
+    this.letter = letter;
+  }
+
+  getLetter(e) {
+    for (let i = 0; i < 26; i++) {
+      const label = (i + 10).toString(36);
+      const btn = document.createElement('button');
+      btn.innerHTML = label;
+      this.letter.appendChild(btn);
+    }
+  }
   start() {
     console.log('start game');
+    this.getLetter();
   }
 }
 
-const game = new Game();
+const game = new Game({ word: document.querySelector('div.word'), letter: document.querySelector('div.letter') });
 game.start();
